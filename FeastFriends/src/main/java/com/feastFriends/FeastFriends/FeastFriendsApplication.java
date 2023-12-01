90bfb94bef16f68ab64c32e0f912f22bdf27ec5b
Madduxv <madduxvjvitrano@gmail.com>
2023-11-30 22:02:07 -0600

    initial commit

---
+++ FeastFriends/src/main/java/com/feastFriends/FeastFriends/FeastFriendsApplication.java
@@ -0,0 +1,13 @@
+package com.feastFriends.FeastFriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeastFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeastFriendsApplication.class, args);
+	}
+
+}
---
+++ FeastFriends/src/test/java/com/feastFriends/FeastFriends/FeastFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feastFriends.FeastFriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeastFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

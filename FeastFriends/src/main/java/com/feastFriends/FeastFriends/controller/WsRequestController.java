20118378bed89f0c28d4c9cff471cd079be77d10
Madduxv <madduxvjvitrano@gmail.com>
2024-07-18 19:57:33 -0500

    Adding api endpoints for websockets

--- FeastFriends/src/main/java/com/feastFriends/FeastFriends/controller/WsRequestController.java
+++ FeastFriends/src/main/java/com/feastFriends/FeastFriends/controller/WsRequestController.java
@@ -2,6 +2,7 @@ package com.feastFriends.feastFriends.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,25 +14,30 @@ import com.feastFriends.feastFriends.websockets.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/ws")
 public class WsRequestController {
 
-    @Autowired
-    private WebSocketHandler webSocketHandler;
+  @Autowired
+  private WebSocketHandler webSocketHandler;
 
-    @GetMapping("/getSessionMessages")
-    public ResponseEntity<List<String>> getSessionMessages(@RequestBody String ownerName) {
-        WebSocketSession session = findSessionById(ownerName.trim()+"'s Group"); // Implement a method to find session by ID
-        if (session != null) {
-            List<String> messages = webSocketHandler.getMessagesForSession(session);
-            return ResponseEntity.ok(messages);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+  @GetMapping("/getSessionMessages")
+  public ResponseEntity<List<String>> getSessionMessages(@RequestBody String ownerName) {
+    WebSocketSession session = findSessionById(ownerName.trim()+"'s Group"); // Implement a method to find session by ID
+    if (session != null) {
+      List<String> messages = webSocketHandler.getMessagesForSession(session);
+      return ResponseEntity.ok(messages);
+    } else {
+      return ResponseEntity.notFound().build();
     }
+  }
 
-    private WebSocketSession findSessionById(String sessionId) {
-        // Implement logic to retrieve WebSocketSession by its ID or other identifier
-        return null;
-    }
+  @PostMapping("/request_genre")
+  public ResponseEntity<String> requestGenre(String genre) {
+    return ResponseEntity.ok("Successfully requested genre");
+  }
+
+  private WebSocketSession findSessionById(String sessionId) {
+    // Implement logic to retrieve WebSocketSession by its ID or other identifier
+    return null;
+  }
 }

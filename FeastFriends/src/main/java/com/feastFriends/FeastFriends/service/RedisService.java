57221ffe952b0b355848823c907e31665efcd1dd
Madduxv <madduxvjvitrano@gmail.com>
2024-08-29 12:39:14 -0500

    Adding mini-redis integration

---
+++ FeastFriends/src/main/java/com/feastFriends/FeastFriends/service/RedisService.java
@@ -0,0 +1,12 @@
+package com.feastFriends.feastFriends.service;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class RedisService {
+  private Socket socket;
+  private PrintWriter out;
+  private BufferedReader in;
+
+}
